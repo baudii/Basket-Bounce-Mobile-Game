4813cf92ef7b35bb084e23eb0b402acbc2ca9759
yanerobot <screwyuguys@gmail.com>
2024-11-23 06:13:16 +0300

    Created new mechanic - RigidBodyInteractable

---
+++ Assets/__Scripts/Gameplay/Mechanics/RigidBodyInteractable.cs
@@ -0,0 +1,57 @@
+using BasketBounce.Systems;
+using KK.Common;
+using System.Threading;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+namespace BasketBounce.Gameplay
+{
+	[RequireComponent(typeof(Rigidbody2D))]
+	public class RigidBodyInteractable : MonoBehaviour, IResetableItem
+    {
+		Rigidbody2D rb;
+
+		Vector3 initialPosition;
+		bool isFalling;
+		bool hasFallen;
+		float startTime;
+		private void Start()
+		{
+			initialPosition = transform.position;
+			rb = GetComponent<Rigidbody2D>();
+			startTime = Time.time;
+		}
+
+		private void FixedUpdate()
+		{
+			if (Time.time - startTime < 0.5f)
+				return;
+
+            if (hasFallen)
+				return;
+
+            if (isFalling && rb.velocity.magnitude <= 0.1f)
+			{
+				isFalling = false;
+				rb.isKinematic = true;
+				rb.velocity = Vector3.zero;
+				hasFallen = true;
+				this.Log("Has fallen");
+			}
+			else if (!isFalling && rb.velocity.magnitude > 0.1f)
+			{
+				this.Log("Started falling");
+				isFalling = true;
+			}
+		}
+
+		public void ResetState()
+		{
+			transform.position = initialPosition;
+			rb.isKinematic = false;
+			isFalling = false;
+			hasFallen = false;
+			startTime = Time.time;
+		}
+    }
+}
